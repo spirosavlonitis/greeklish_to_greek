49adf6ded13718da33591552b2368ac80eda3858
Spiros Avlonitis <spirosa84@hotmail.com>
2019-01-21 04:05:35 +0200

    no errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
-
-
 
 class App extends Component {
 
--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 
+
 if (module.hot)
 	module.hot.accept()
