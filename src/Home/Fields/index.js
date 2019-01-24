29532d6f73366bad3cc0a2f3ab8106007fd7a110
Spiros Avlonitis <spirosa84@hotmail.com>
2019-01-25 01:06:49 +0200

    alphabet

--- src/Home/Fields/index.js
+++ src/Home/Fields/index.js
@@ -3,6 +3,7 @@ import TopBarProgress from "react-topbar-progress-indicator";
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import FormControl from 'react-bootstrap/lib/FormControl';
+import Panel from 'react-bootstrap/lib/Panel';
 import axios from 'axios';
 import greek_words from './Greek.dic';
 import "./index.css"
@@ -396,8 +397,24 @@ export default class Fields extends Component {
 							</div>
 						</div>
 					</div>
+			        <Panel id="collapsible-panel-example-2">
+			          <Panel.Heading>
+			            <Panel.Title toggle>
+			             Click for  Greeklish alphabet
+			            </Panel.Title>
+			          </Panel.Heading>
+			          <Panel.Collapse>
+			            <Panel.Body>
+			              Anim pariatur cliche reprehenderit, enim eiusmod high life
+			              accusamus terry richardson ad squid. Nihil anim keffiyeh
+			              helvetica, craft beer labore wes anderson cred nesciunt sapiente
+			              ea proident.
+			            </Panel.Body>
+			          </Panel.Collapse>
+			        </Panel>
 				</div>
-		</div>
+ 
+      </div>
 		)
 	}
 }
