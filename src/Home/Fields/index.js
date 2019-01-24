d9e02cedeadf59658ca3ac683c4b4c26d302aafa
Spiros Avlonitis <spirosa84@hotmail.com>
2019-01-24 08:42:55 +0200

    capital suggest

--- src/Home/Fields/index.js
+++ src/Home/Fields/index.js
@@ -124,15 +124,15 @@ export default class Fields extends Component {
 		 	if (word.match(/(Τ|τ)ηρ/)){
 		 		best_match.push(this.tone_word(word.replace(/(Τ|τ)ηρ/, 'θρ').toLowerCase(), word_list, true));
 		 		best_match.push(this.tone_word(word.toLowerCase(), word_list, true));
-			 	best_match = best_match.map(s => s[0].toUpperCase() + s.substring(1)).join('/');
 			 	if (suggest === false){
+			 		best_match = best_match.map(s => s[0].toUpperCase() + s.substring(1)).join('/');
 			 		return best_match;
 			 	}
 		 	}else if(word.match(/(Θ|θ)ρ/)){
 		 		best_match.push(this.tone_word(word.replace(/(Θ|θ)ρ/, 'τηρ').toLowerCase(), word_list, true));
 		 		best_match.push(this.tone_word(word.toLowerCase(), word_list, true));
-			 	best_match = best_match.map(s => s[0].toUpperCase() + s.substring(1)).join('/');
 			 	if (suggest === false){
+			 		best_match = best_match.map(s => s[0].toUpperCase() + s.substring(1)).join('/');
 			 		return best_match;
 			 	}
 		 	}else
